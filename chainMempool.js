5016f764bb9df6c1b02c06478d9e40c5a7c07a62
Ashish Soni <ashjas@gmail.com>
2018-12-02 15:17:51 +0530

    POST,requestValidation & message-signature/validate + AddBlock considering authorized or not DONE

---
+++ chainMempool.js
@@ -0,0 +1,112 @@
+/* ===== Mempool Class ==============================
+|  Class with a constructor for chain Mempool 	    |
+|  ===============================================*/
+
+var bitcoinMessage = require('bitcoinjs-message');
+var validationWindow = 60;
+class validationRequest {
+    constructor(address) {
+        this.address = address;
+        this.timeStamp = new Date().getTime().toString().slice(0, -3);
+        this.message = this.address + ':' + this.timeStamp + ':' + 'starRegistry';
+        this.authorized = false;
+    }
+}
+class MemPool {
+    constructor(data) {
+        this.memPool = [];
+    }
+
+    addReqToPool(req) {
+        let response;
+        var alreadyAuthorized = false;
+        var found = this.memPool.some(function (element) {
+            if (element.address == req.address) {
+                if (element.authorized == false)
+                    element = req;//same wallet request rewrites the previous validation request.
+                else
+                    alreadyAuthorized = true;
+                return true;
+            }
+            return false;
+        });
+        if (!found) {
+            this.memPool.push(req);
+            console.log('pushed new to mempool');
+        }
+        if (alreadyAuthorized == false) {
+            response = {
+                address: req.address,
+                message: req.message,
+                requestTimestamp: req.timeStamp,
+                validationWindow: validationWindow - (new Date().getTime().toString().slice(0, -3) - req.timeStamp)
+            };
+        }
+        else {
+            response = {
+                message: 'Authorized request already pending in mempool. You can register a star.'
+            };
+        }
+        return JSON.stringify(response);
+    }
+    isAuthorized(walletAddress) {
+        var idx = 0;
+        var obj = this;
+        return this.memPool.some(function (element) {
+            if (element.address == walletAddress && element.authorized) {
+                obj.memPool.splice(idx, 1);//New star will be registered in blockchain,so now remove the validated request from mempool
+                console.log('Removed validated req, mempool size: ' + obj.memPool.length);
+                return true;
+            }
+            idx++;
+            return false;
+        });
+    }
+    verifySignedMessage(msg) {
+        let response;
+        var idx = 0;
+        console.log('arrsize: ' + this.memPool.length);
+        var obj = this;
+        var found = this.memPool.some(function (element) {
+            if (element.address == msg.walletAddress && element.authorized) {
+                response = {
+                    message: 'You are already authorized to register a star, skipping signature verification.'
+                };
+                return true;
+            }
+            else if (element.address == msg.walletAddress) {
+                if ((validationWindow - (new Date().getTime().toString().slice(0, -3) - element.timeStamp)) > 0) {//within validation window.
+                    if (bitcoinMessage.verify(element.message, msg.walletAddress, msg.signature)) {
+                        element.authorized = true;
+                        response = {
+                            message: 'Verification Success. Can proceed to register a Star.'
+                        };
+                    }
+                    else {
+                        response = {
+                            message: 'Verification Failed, check your signature. You can\'t register a Star.'
+                        };
+                    }
+                }
+                else {// Remove the validation request if window expired.
+                    obj.memPool.splice(idx, 1);
+                    response = {
+                        message: 'Validation window expired, add another request to mempool before verification.'
+                    };
+                }
+                return true;
+            }
+            idx++;
+            return false;
+        });
+        if (!found) {
+            response = {
+                message: 'Request not available in mempool.'
+            };
+        }
+        return JSON.stringify(response);
+    }
+}
+
+module.exports.MemPool = MemPool;
+module.exports.validationRequest = validationRequest;
--- serveRest.js
+++ serveRest.js
@@ -2,9 +2,10 @@
 
 var Blockchain = require('./simpleChain');
 var Block = require('./Block');
+var ChainPool = require('./chainMempool');
 
 const Hapi=require('hapi');
-
+var MemPool = new ChainPool.MemPool();
 class restERROR{
     constructor(code, msg) {
         this.code = code,
@@ -44,22 +45,64 @@ server.route([{
 {
     method: 'POST',
     path: '/block',
-    handler:async function(request,h) {
+    handler: async function (request, h) {
         try {
             let Chain = new Blockchain.Blockchain();
             var blockData = request.payload.body;
-            if(blockData){
-                var out = await Chain.addBlock(new Block.Block(blockData));
+            var walletAddress = request.payload.walletAddress;
+            if (blockData) {
+                if (MemPool.isAuthorized(walletAddress)) {
+                    var out = await Chain.addBlock(new Block.Block(blockData));
+                    return JSON.parse(out);
+                }
+                else
+                    return new restERROR(1502, 'Not authorized to add a block.').getJson();
+            }
+            else
+                return new restERROR(1503, 'Request parameter does not have \'body\' field, which is required for adding a block.').getJson();
+        }
+        catch (error) {
+            return 'Exception in POST handler: ' + error;
+        }
+    }
+},
+{
+    method: 'POST',
+    path: '/requestValidation',
+    handler:async function(request,h) {
+        try {
+            var walletAddress = request.payload.walletAddress;
+            if(walletAddress){
+                var out = await MemPool.addReqToPool(new ChainPool.validationRequest(walletAddress));
                 return JSON.parse(out);
             }
             else
-                return new restERROR(1500,'Request parameter does not have \'body\' field, which is required for adding a block.').getJson();
+                return new restERROR(1504,'Request parameter does not have \'walletAddress\' field, which is required for adding a block.').getJson();
         }
         catch(error) {
             return 'Exception in POST handler: ' + error;          
         }
     }
-}]);
+},
+{
+    method: 'POST',
+    path: '/message-signature/validate',
+    handler:async function(request,h) {
+        try {
+            var signature = request.payload.signature;
+            if(signature){
+                var out = await MemPool.verifySignedMessage(request.payload);
+                return JSON.parse(out);
+            }
+            else
+                return new restERROR(1505,'Request parameter does not have \'signature\' field, which is required for adding a block.').getJson();
+        }
+        catch(error) {
+            return 'Exception in POST handler: ' + error;          
+        }
+    }
+}
+]);
 
 // Start the server
 async function start() {
